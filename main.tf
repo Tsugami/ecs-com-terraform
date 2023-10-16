745c770766c5dba12e5f7ee6fb8acad105a6675f
Yslan Ramos <yslanramos.r@gmail.com>
2023-10-16 11:38:40 -0300

    add common tags

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = var.region
 }
+
+
+locals {
+  tags = {
+    ManagedBy  = "Terraform"
+    Enviroment = var.env
+    Repository = "https://github.com/Tsugami/alura-ecs-com-terraform"
+  }
+}
