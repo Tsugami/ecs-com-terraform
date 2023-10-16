4db82945324a2bf4ab1d1a75fb2c414a63511764
Yslan Ramos <yslanramos.r@gmail.com>
2023-10-16 17:50:02 -0300

    Add Application Load Balancing

--- ecs.tf
+++ ecs.tf
@@ -33,25 +33,36 @@ resource "aws_security_group" "ecs" {
   }
 }
 
+data "aws_ssm_parameter" "ecs_optimized_ami" {
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended"
+}
+
 resource "aws_launch_template" "ecs" {
-  name_prefix            = local.name
-  image_id               = data.aws_ami.ubuntu.id
+  name_prefix            = "ecs-template"
+  image_id               = jsondecode(data.aws_ssm_parameter.ecs_optimized_ami.value)["image_id"]
   instance_type          = var.instance_type
+  # key_name               = "ec2ecsglog"
   vpc_security_group_ids = [aws_security_group.ecs.id]
   user_data = base64encode(<<-EOT
         #!/bin/bash
-        ECS_CLUSTER=${local.name}
-        ECS_LOGLEVEL=debug
-        ECS_CONTAINER_INSTANCE_TAGS=${jsonencode(local.tags)}
-        ECS_ENABLE_TASK_IAM_ROLE=true
-        EOF
+        echo ECS_CLUSTER=my-ecs-cluster >> /etc/ecs/ecs.config
       EOT
   )
+
+  iam_instance_profile {
+    name = "ecsInstanceRole"
+  }
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "ecs-instance"
+    }
+  }
 }
 
 resource "aws_autoscaling_group" "ecs_sg" {
   name             = "${local.name}-ecs-sg"
-  desired_capacity = 2
+  desired_capacity = 3
   max_size         = 3
   min_size         = 2
 
@@ -119,16 +130,53 @@ resource "aws_ecs_cluster_capacity_providers" "ec2" {
   }
 }
 
+
+
 resource "aws_ecs_task_definition" "nginx" {
   family                = "${local.name}-nginx"
+  network_mode          = "awsvpc"
+  cpu                   = 256
   container_definitions = file("./task-definitions/nginx.json")
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
 }
 
 resource "aws_ecs_service" "nginx" {
   name            = "${local.name}-nginx"
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.nginx.arn
-  desired_count   = 1
+  desired_count   = 2
+
+
+  capacity_provider_strategy {
+    weight            = 100
+    capacity_provider = aws_ecs_capacity_provider.main.name
+  }
+
+  load_balancer {
+    container_name   = "nginx"
+    container_port   = 80
+    target_group_arn = aws_lb_target_group.ecs.arn
+  }
+
+  force_new_deployment = true
+  placement_constraints { // oq isso faz?
+    type = "distinctInstance"
+  }
+
+  network_configuration {
+    subnets         = [var.subnet_1a, var.subnet_1b]
+    security_groups = [aws_security_group.ecs.id]
+  }
+
+  triggers = {
+    redeployment = timestamp()
+  }
+
+  depends_on = [aws_autoscaling_group.ecs_sg]
 }
 
 resource "aws_lb" "ecs" {
@@ -141,8 +189,21 @@ resource "aws_lb" "ecs" {
 }
 
 resource "aws_lb_target_group" "ecs" {
-  name     = "${local.name}-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.main.id
+  name        = "${local.name}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = data.aws_vpc.main.id
+}
+
+resource "aws_lb_listener" "ecs" {
+  load_balancer_arn = aws_lb.ecs.arn
+  port              = 80
+  protocol          = "HTTP"
+
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs.arn
+  }
 }
